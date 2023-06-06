de149460ac94e1e71b54cdd01bddcfa8983e1ee4
Dave V <dave@mindkeep.org>
2023-06-06 14:20:52 -0600

    configure nvim-osc52 for copy paste functionality

--- src/config/nvim/lua/plugins/nvim-osc52.lua
+++ src/config/nvim/lua/plugins/nvim-osc52.lua
@@ -1,3 +1,20 @@
 return {
-	{ "ojroques/nvim-osc52" },
+	{
+		"ojroques/nvim-osc52",
+		config = function(_)
+			local function copy(lines, _)
+				require("osc52").copy(table.concat(lines, "\n"))
+			end
+
+			local function paste()
+				return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+			end
+
+			vim.g.clipboard = {
+				name = "osc52",
+				copy = { ["+"] = copy, ["*"] = copy },
+				paste = { ["+"] = paste, ["*"] = paste },
+			}
+		end,
+	},
 }
