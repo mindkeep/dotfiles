49f046a0de6ad150b40845a7b7aab70353afedcb
Dave V <dave@mindkeep.org>
2023-06-06 10:47:47 -0600

    add a few more useful looking mason packages

--- src/config/nvim/lua/plugins/coding.lua
+++ src/config/nvim/lua/plugins/coding.lua
@@ -3,13 +3,22 @@ return {
 		"williamboman/mason.nvim",
 		opts = function(_, opts)
 			vim.list_extend(opts.ensure_installed, {
+				"ansible-language-server",
 				"bash-language-server",
 				"clangd",
+				"cpplint",
+				"cpptools",
+				"docker-compose-language-service",
 				"dockerfile-language-server",
 				"gopls",
+				"jedi-language-server",
+				"json-lsp",
+				"jq",
 				"lua-language-server",
+				"prettier",
 				"pyright",
 				"yaml-language-server",
+				"yq",
 			})
 		end,
 	},
